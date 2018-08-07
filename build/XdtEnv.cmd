78bde90494e9beee8aa0d4d78f5f4c2f200fd1c8
vramak <vramak@microsoft.com>
2018-08-07 13:27:06 -0700

    Adding msbuild and dotnet to path

--- build/XdtEnv.cmd
+++ build/XdtEnv.cmd
@@ -14,5 +14,21 @@ if not defined FeedTasksPackageVersion (
 
 set "XdtRoot=%~dp0"
 set "XdtRoot=%XdtRoot:~0,-7%"
-
 set "PATH=%PATH%;%XdtRoot%"
+
+
+if defined ProgramFiles(x86) (
+    set "XdtProgramFiles=%ProgramFiles(x86)%"
+) else (
+    set "XdtProgramFiles=%ProgramFiles%"
+)
+
+if exist "%XdtProgramFiles%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
+    set "XdtMSBuildPath=%XdtProgramFiles%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"
+) else (
+    set "XdtMSBuildPath=%XdtProgramFiles%\MSBuild\14.0\Bin"
+)
+
+set "PATH=%PATH%;%XdtMSBuildPath%"
+
+set "PATH=%PATH%;%ProgramFiles%\dotnet\;"
