45f39703e581934ef45a91ba0a5c7201f03856e6
secopsinternal <125038851+secopsinternal@users.noreply.github.com>
2023-02-10 19:24:31 +0530

    Update XmlAttributePreservingWriter.cs

--- src/Microsoft.Web.XmlTransform/XmlAttributePreservingWriter.cs
+++ src/Microsoft.Web.XmlTransform/XmlAttributePreservingWriter.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Xml;
 using System.IO;
+using System.Xml;
 using System.Diagnostics;
 using System.Globalization;
 
